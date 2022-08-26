0e97e2f2f95237d2e7aa7df19b94dac65beaa508
barnau21 <adumitriu21@gmail.com>
2022-08-26 09:23:33 +0300

    Initial Commit

---
+++ Shared/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  Shared
+//
+//  Created by Adrian Dumitriu2 on 26.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
